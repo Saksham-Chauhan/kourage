5c275b711277d2c993c45c552fb407907b5ef915
bhavanshuarora <63899693+bhavanshuarora@users.noreply.github.com>
2021-07-22 13:40:52 +0530

    Update discord_msg_sentimentAnalysis.py

--- discord_msg_sentimentAnalysis.py
+++ discord_msg_sentimentAnalysis.py
@@ -3,76 +3,47 @@ import csv
 import pandas as pd
 import datetime
 from discord.ext import tasks, commands
-#from discord import channel
-token = 'ODY2NjA2OTE5OTYyOTg0NDg4.YPVAlg.b-BxA2WyJJaGZ2ZV3kbyBSSKv0Q'
-#bot = commands.Bot(command_prefix='!')
-# class MyClient(discord.Client):
+token = '<Add token here>'  # bot token to be added here
 intents = discord.Intents.all()
 intents.reactions = True
 intents.members = True
 intents.guilds = True
-
 class MyBot(commands.Bot):
     def __init__(self, command_prefix, self_bot, intent):
         commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot)
-        #self.message_history()
 
-    #@commands.bot.event
     async def on_ready(self):
         print('Logged on as', self.user)
         self.bot_send_cmd.start()
         print(self)
-        #message_history.start()
-        #channel = client.get_channel("848837318244565013")
 
     @tasks.loop(hours=24)
     async def bot_send_cmd(self):
-        channel = self.get_channel(854930810688110605)
+        channel = self.get_channel(854930810688110605)  #enter channel id Int format
         print(channel)
         await channel.send('!99')
 
-    # @bot.command(name='99')
-    #@commands.bot.command(name='99')
-    #@tasks.loop(hours=24)
-    # def message_history(self):
-    #     @self.command()
     async def on_message(self,ctx):
         if ctx.author == bot.user and ctx.content=='!99':
             print("Hello")
             data = pd.DataFrame(columns=['content', 'time', 'author'])
-            async for msg in ctx.channel.history(limit=1000):
-                  # set high limit
-            # As an example, I've set the limit to 1000
-            # meaning it'll read 10000 messages instead of
-            # the default amount of 1000
+            async for msg in ctx.channel.history(limit=1000):           # set high limit
                 print(msg)
             # Trigger to fetch current date messages
                 Flag = True
             # comparison of created_at sate and current date
                 if(msg.created_at.strftime('%Y-%m-%d') == datetime.date.today().strftime('%Y-%m-%d')):
-            # print("hello")
                     data = data.append({'content': msg.content, 'time': msg.created_at, 'author': msg.author.id}, ignore_index=True)
-
                 else:
                     Flag = False  # if date doesn't it means that its the previous day so we break the loop
 
                 if(Flag == False):
-                # Set the string to where you want the file to be saved to
-                    file_location = "summer_intern.csv"
-
+                    file_location = "summer_intern.csv"   # Set the string to where you want the file to be saved to
                 # DataFrame creates index, which is not required while storing file in csv format
                     data.to_csv(file_location, index=False)
-                # print("heloooooo")
                 #sentiment analysis
                 self.sentimentAnalysis()          # in case of await function runs infinitly
             await ctx.channel.send(file=discord.File('summer-intern.png'))
-
-                # break
-        # file_location = "msg-history.csv" # Set the string to where you want the file to be saved to
-
-        # data.to_csv(file_location,index=False)  #DataFrame creates index, which is not required while storing file in csv format
-        # return
-        print("mkhc")
 
     async def sentimentAnalysis(self):
         import nltk
@@ -116,23 +87,7 @@ class MyBot(commands.Bot):
         df = data1.groupby(['author','compound']).size().unstack(fill_value=0)
         df1 = df.iloc[:, 0:].apply(lambda x: x.div(x.sum()).mul(100), axis=1).astype(int)
         df1.plot(kind='bar')
-        print("-------------------------------------")
         plt.savefig('summer-intern.png')
-        #plt.show()
         return
-
-        #
-
-
-    # async def name(self,ctx):
-    #     print("kshn ")
-    #     #await ctx.channel.send('books_read.png')
-    #     await ctx.channel.send(file=discord.File('books_read.png'))
-    # @client.command()
-    # async def info(self,ctx):
-    #     client.loop.create_task(message_history(ctx))
 bot = MyBot(command_prefix='!', self_bot=False, intent=intents)
-# client.info.start()
-# client.message_history.start()
-# client.run(token)
 bot.run(token)
