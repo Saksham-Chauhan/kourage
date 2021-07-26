b5ebbd171f84957d311647f3d802ad2cb9755b2b
bhavanshuarora <63899693+bhavanshuarora@users.noreply.github.com>
2021-07-26 11:15:25 +0530

    Add files via upload

--- discord_msg_sentimentAnalysis.py
+++ discord_msg_sentimentAnalysis.py
@@ -3,47 +3,41 @@ import csv
 import pandas as pd
 import datetime
 from discord.ext import tasks, commands
-token = '<Add token here>'  # bot token to be added here
+token = 'ODY2NjA2OTE5OTYyOTg0NDg4.YPVAlg.dkbGE5UHFBiQmFt_qC_umxRZEU0'  # bot token to be added here
 intents = discord.Intents.all()
 intents.reactions = True
 intents.members = True
 intents.guilds = True
 class MyBot(commands.Bot):
+
+    channels = [862219851850317845]
+
     def __init__(self, command_prefix, self_bot, intent):
         commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot)
 
     async def on_ready(self):
         print('Logged on as', self.user)
-        self.bot_send_cmd.start()
-        print(self)
+        self.message_history.start()
 
     @tasks.loop(hours=24)
-    async def bot_send_cmd(self):
-        channel = self.get_channel(854930810688110605)  #enter channel id Int format
-        print(channel)
-        await channel.send('!99')
-
-    async def on_message(self,ctx):
-        if ctx.author == bot.user and ctx.content=='!99':
-            print("Hello")
-            data = pd.DataFrame(columns=['content', 'time', 'author'])
-            async for msg in ctx.channel.history(limit=1000):           # set high limit
+    async def message_history(self):
+        data = pd.DataFrame(columns=['content', 'time', 'author'])
+        for ch in self.channels:
+            channel = self.get_channel(ch)
+            print(channel)
+            channel.history(limit=10)
+            async for msg in channel.history(limit=1000):           # set high limit
                 print(msg)
-            # Trigger to fetch current date messages
-                Flag = True
-            # comparison of created_at sate and current date
+                Flag = True    # Trigger to fetch current date messages
                 if(msg.created_at.strftime('%Y-%m-%d') == datetime.date.today().strftime('%Y-%m-%d')):
-                    data = data.append({'content': msg.content, 'time': msg.created_at, 'author': msg.author.id}, ignore_index=True)
+                    data = data.append({'content': msg.content, 'time': msg.created_at, 'author': msg.author.name}, ignore_index=True)
                 else:
                     Flag = False  # if date doesn't it means that its the previous day so we break the loop
-
                 if(Flag == False):
-                    file_location = "summer_intern.csv"   # Set the string to where you want the file to be saved to
-                # DataFrame creates index, which is not required while storing file in csv format
+                    file_location = "summer_intern.csv"   
                     data.to_csv(file_location, index=False)
-                #sentiment analysis
-                self.sentimentAnalysis()          # in case of await function runs infinitly
-            await ctx.channel.send(file=discord.File('summer-intern.png'))
+                self.sentimentAnalysis()          
+            await channel.send(file=discord.File('summer-intern.png'))
 
     async def sentimentAnalysis(self):
         import nltk
@@ -87,7 +81,10 @@ class MyBot(commands.Bot):
         df = data1.groupby(['author','compound']).size().unstack(fill_value=0)
         df1 = df.iloc[:, 0:].apply(lambda x: x.div(x.sum()).mul(100), axis=1).astype(int)
         df1.plot(kind='bar')
+        plt.xticks(range(df1.shape[0]),df1['author'],rotation=90)
+        plt.tight_layout()
         plt.savefig('summer-intern.png')
         return
 bot = MyBot(command_prefix='!', self_bot=False, intent=intents)
 bot.run(token)
+
