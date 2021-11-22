b7ec6cfd3ce383e0cf674cfe0631f2f4ad17b291
xhunter <kartikey@koders.in>
2021-11-22 14:04:35 +0530

    Added open issues embed

---
+++ process/tickets/embed.py
@@ -0,0 +1,17 @@
+from helper.embed_props import *
+import discord
+
+
+def get_open_issues_embed(data):
+    header_text = "**Open Issues**"
+    embed = discord.Embed(
+        description="**Please clear these issues asap** \n" + data,
+        timestamp=timestamp,
+        color=color
+    )
+    embed.set_author(name=header_text)
+    embed.set_footer(text=footer_text,
+                     icon_url=icon_url)
+
+    return embed
+
