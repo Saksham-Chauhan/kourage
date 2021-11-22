f64dfe4e8143d5c7022735ea7ff011f7e173e52f
xhunter <kartikey@koders.in>
2021-11-22 13:57:01 +0530

    ➕ Added prepare_embed_message for tickets

--- process/tickets/main.py
+++ process/tickets/main.py
@@ -1,4 +1,5 @@
 from helper.redmine import RedmineConfig
+from redminelib.exceptions import ResourceAttrError
 
 
 # TODO => Show opened issues
@@ -8,16 +9,22 @@ from helper.redmine import RedmineConfig
 
 def get_open_issues():
     redmine = RedmineConfig().initialize()
-
     issues = redmine.issue.filter(status='open')
     return issues
 
 
 def get_resolved_issues():
     redmine = RedmineConfig().initialize()
-
     issues = redmine.issue.filter(status='resolved')
     return issues
 
 
+def prepare_embed_message(issues):
+    message = str()
+    for issue in issues:
+        try:
+            message += issue['id'] + " - " + issues['subject'] + " - " + issues['assigned_to']
+        except ResourceAttrError:
+            message += issue['id'] + " - " + issues['subject'] + " - " + "_Please assign this_"
+    return message
 
